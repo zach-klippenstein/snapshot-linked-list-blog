f7d2edab63294ac4bada02175b7fdcb6ad86b1cd
Zach Klippenstein <zach.klippenstein@gmail.com>
2021-04-29 07:25:45 -0700

    Initial code and project commit.

---
+++ src/main/kotlin/SnapshotList.kt
@@ -0,0 +1,16 @@
+class SnapshotList {
+
+    val count: Int = TODO()
+
+    fun addFirst(item: Any?) {
+        TODO()
+    }
+
+    fun addLast(item: Any?) {
+        TODO()
+    }
+
+    fun contains(item: Any?) {
+        TODO()
+    }
+}
---
+++ src/test/kotlin/SnapshotListTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class SnapshotListTest {
+
+    @Test
+    fun stuff() {
+
+    }
+}
